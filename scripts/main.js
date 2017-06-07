646ae35f1e9df98a63163e67f29dfa8862e6da13
evan miller <emille4@clemson.edu>
2017-06-07 08:59:48 -0400

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,15 @@
+(function (){
+  'use strict';
+
+  var arrowWrapper =   document.querySelector('.arrow-wrapper');
+  var psWrapper = document.querySelector(".ps-wrapper");
+
+  arrowWrapper.style.display = "none";
+
+  psWrapper.onmouseover = function(){
+      arrowWrapper.style.display = "inline-block";
+  }
+  psWrapper.onmouseout = function(){
+      arrowWrapper.style.display = "none";
+  }
+})();
